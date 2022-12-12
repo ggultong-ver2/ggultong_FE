1f771a0f1e0f165daa48250bad311389d4ab5f6d
saeyeonKim <118176015+saeyeonKim@users.noreply.github.com>
2022-12-12 15:23:52 +0900

    saeyeon first commit

---
+++ src/pages/RecipeList.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const RecipeList = () => {
+  return (
+    <div>
+      <h1> Recipe Community</h1>
+    </div>
+  );
+};
+
+export default RecipeList;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "../components/layout/Layout";
 // 주석 풀고 사용
-// import RecipeList from "../pages/RecipeList";
+import RecipeList from "../pages/RecipeList";
 // import Recipe from "../components/recipe/Recipe";
 
 const Router = () => {
@@ -13,8 +13,8 @@ const Router = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           {/* 주석 풀고 사용 */}
-          {/* <Route path="lists" element={<RecipeList />} />
-          <Route path="lists/:id" element={<Recipe />} /> */}
+          <Route path="lists" element={<RecipeList />} />
+          {/* <Route path="lists/:id" element={<Recipe />} /> */}
         </Routes>
       </Layout>
     </BrowserRouter>
