16bf65032572a525dfc3e0318e75fd9f459ba42c
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2022-12-14 22:33:57 +0900

    list 일부수정

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const List = ({ recipelist }) => {
   const navigate = useNavigate();
   const { title, imgurl, recipe, id } = recipelist;
-
+  // list id 넘겨오기
   return (
     <Cardcontainer>
       <div>
@@ -23,7 +23,7 @@ const List = ({ recipelist }) => {
         <Stbutton
           borderColor="#ddd"
           onClick={() => {
-            navigate(`/lists/${[id]}`);
+            navigate(`/lists/${[id]}`); // [id].배열 보내기
           }}
         >
           View More
