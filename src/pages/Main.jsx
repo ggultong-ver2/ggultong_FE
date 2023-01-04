5db10e564c15f2ccda48f0eb01d19f369e8a0b20
lidieee <seb384@naver.com>
2023-01-04 13:17:09 +0900

    main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/header/Header";
+import "./reset.css"
+import "./style.css"
 
 function Main() {
   return (
     <>
       <Header />
-      메인페이지
+      <div className="main_container">메인페이지</div>
     </>
   );
 }
