aa4b82eddcc03d94863854f2793378d6336c15c0
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2023-01-07 04:28:03 +0900

    로그인회원가입완료

--- src/redux/modules/checkIdSlice.js
+++ src/redux/modules/checkIdSlice.js
@@ -7,17 +7,14 @@ const __idcheck = async (loginId) => {
     const data = await apis.checkUserName(loginId);
     console.log("loginId:::", loginId);
     console.log("data: ", data);
-    if (data.data.statusCode === 200) {
+    if (data.data.status === 200) {
+      console.log("response", data.response);
       // alert(Swal.fire("사용가능한 ID", "사용가능합니다!", "success"));
     }
     // useSweet(1000, "success", "회원가입 성공");
     return data;
   } catch (error) {
-    Swal.fire(
-      error.response.data.msg,
-      "다른 아이디를 사용 해 주세요!",
-      "error"
-    );
+    Swal.fire("dd", "다른 아이디를 사용 해 주세요!", "error");
     // alert(error.response.data.msg);
     // useSweet(1000, "error", error.response.data.msg);
   }
