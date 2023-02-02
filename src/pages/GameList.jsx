90205541e04de1d905d872229ce30184735268e6
lidieee <seb384@naver.com>
2023-02-03 01:48:36 +0900

    design : gamelist

--- src/pages/GameList.jsx
+++ src/pages/GameList.jsx
@@ -196,11 +196,13 @@ const StRightBox2 = styled.div`
 
 const StListBox = styled.div`
   margin: auto;
-  /* padding-left: 23px; */
   font-size: 28px;
   font-weight: 600;
   width: 1200px;
-  height: 900px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-bottom: 100px;
 `;
 
 const StFoodBox = styled.div`
@@ -272,12 +274,11 @@ const StImgBox2 = styled.img`
 `;
 
 const StCard = styled.div`
-  margin-right: 16px;
   width: 384px;
   height: 255px;
   font-size: 18px;
   font-weight: 500;
-  font-family: "Pretendard";
+  margin-top: 40px;
 `;
 const StP = styled.p`
   background-color: silver;
@@ -294,7 +295,6 @@ const StDiv = styled.div`
   margin-bottom: 30px;
   width: 1200px;
   height: 50px;
-  font-family: "Pretendard";
   font-weight: 600;
   font-size: 28px;
   padding-top: 20px;
