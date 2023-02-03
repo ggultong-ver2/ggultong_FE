c6c6b0b478d8c895d7774da92c8f8908439b8db9
lidieee <seb384@naver.com>
2023-02-04 04:20:53 +0900

    feat : notification

--- src/redux/modules/notificationSlice.js
+++ src/redux/modules/notificationSlice.js
@@ -1,112 +1,118 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { baseURL } from "../../lib/axios";
-import axios from 'axios';
-
+import axios from "axios";
 
 export const __getNotification = createAsyncThunk(
-    'getNotification',
-    async (payload, thunkAPI) => {
-        try {
-            const data = await baseURL.get(`/notifications`)
-            return thunkAPI.fulfillWithValue(data.data)
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error)
-        }
+  "getNotification",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await baseURL.get("/notifications");
+      console.log(data.data)
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-)
+  }
+);
 
-export const notificationSlice = createSlice({
-    name: 'getNotification',
-    initialState:{
-        notifications: {
-            data: [{id: 0, content: {}, status: false}],
-            error: null,
-            success: true,
-        },
-        isLoading: false,
-        error: null,
+export const notification = createSlice({
+  name: "notification",
+  initialState: {
+    notifications: [{ id: 0, content: {}, status: false, url: "" }],
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    __addNotification(state, action) {
+      state.notifications.data.unshift(action.payload);
     },
-    reducers: {
-        __addNotification(state, action) {
-            // console.log(action.payload)
-            state.notifications.data.unshift(action.payload)
-        },
-        __readNotification(state, action) {
-            // console.log(current(state.notifications.data))
-            const a = state.notifications.data.findIndex((v) => v.id === action.payload)
-            state.notifications.data[a].status = true;
-        },
-        __deleteNotification(state, action) {
-            const a = state.notifications.data.findIndex((v) => v.id === action.payload)
-            state.notifications.data.splice(a,1)
-        },
-        __deleteNotification(state, action) {
-            state.notifications.data.splice(0,state.notifications.data.length)
-        }
+    __readNotification(state, action) {
+      const a = state.notifications.data.findIndex(
+        (v) => v.id === action.payload
+      );
+      state.notifications.data[a].status = true;
     },
-    extraReducers: {
-        [__getNotification.pending]: (state) => {
-            state.isLoading = true;
-        },
-        [__getNotification.fulfilled]: (state, action) => {
-            state.isLoading = false;
-            state.notifications = action.payload;
-        },
-        [__getNotification.rejected]: (state, action) => {
-            state.isLoading = false;
-            state.error = action.payload;
-        }
-    }
-})
+    __deleteNotification(state, action) {
+      const a = state.notifications.data.findIndex(
+        (v) => v.id === action.payload
+      );
+      state.notifications.data.splice(a, 1);
+    },
+    __deleteNotification(state, action) {
+      state.notifications.data.splice(0, state.notifications.data.length);
+    },
+  },
+  extraReducers: {
+    [__getNotification.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__getNotification.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.notifications = action.payload;
+      // console.log("action.payload", action.payload)
+    },
+    [__getNotification.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
 
-export const { __addNotification, __readNotification, __deleteNotification, __deleteNotifications } = notificationSlice.actions
+export const {
+  __addNotification,
+  __readNotification,
+  __deleteNotification,
+  __deleteNotifications,
+} = notification.actions;
+
 
 
 
 export const __NreadNotification = createAsyncThunk(
-    'NreadNotification',
-    async (payload, thunkAPI) => {
-        try {
-            const data = await baseURL.get(`/notifications/count`)
-            return thunkAPI.fulfillWithValue(data.data)
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error)
-        }
+  "NreadNotification",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await baseURL.get("/notifications/count");
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
-)
+  }
+);
 
 export const NreadNotificationSlice = createSlice({
-    name: 'NreadNotification',
-    initialState:{
-        NreadNotifications: {
-            data: {count: 0},
-            error: null,
-            success: true,
-        },
-        isLoading2: false,
-        error2: null,
+  name: "NreadNotification",
+  initialState: {
+    NreadNotifications: {
+      data: { count: 0 },
+      error: null,
+      success: true,
     },
-    reducers: {
-        _minusNotification(state, action) {
-            state.NreadNotifications.data.count -= action.payload
-        },
-        _plusNotification(state, action) {
-            state.NreadNotifications.data.count += action.payload
-        }
+    isLoading2: false,
+    error2: null,
+  },
+  reducers: {
+    __minusNotification(state, action) {
+      state.NreadNotifications.data.count -= action.payload;
     },
-    extraReducers: {
-        [__NreadNotification.pending]: (state) => {
-            state.isLoading2 = true;
-        },
-        [__NreadNotification.fulfilled]: (state, action) => {
-            state.isLoading2 = false;
-            state.NreadNotifications = action.payload;
-        },
-        [__NreadNotification.rejected]: (state, action) => {
-            state.isLoading2 = false;
-            state.error2 = action.payload;
-        }
-    }
-})
+    __plusNotification(state, action) {
+      state.NreadNotifications.data.count += action.payload;
+    },
+  },
+  extraReducers: {
+    [__NreadNotification.pending]: (state) => {
+      state.isLoading2 = true;
+    },
+    [__NreadNotification.fulfilled]: (state, action) => {
+      state.isLoading2 = false;
+      state.NreadNotifications = action.payload;
+    },
+    [__NreadNotification.rejected]: (state, action) => {
+      state.isLoading2 = false;
+      state.error2 = action.payload;
+    },
+  },
+});
 
-export const { __minusNotification, __plusNotification } = NreadNotificationSlice.actions
+export const { __minusNotification, __plusNotification } =
+  NreadNotificationSlice.actions;
