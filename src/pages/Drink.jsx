6451e8f63e89c4aa7f5321c48d826ecd1e7f7c8e
lidieee <seb384@naver.com>
2023-02-05 03:00:15 +0900

    design : 메인 페이지

--- src/pages/Drink.jsx
+++ src/pages/Drink.jsx
@@ -1,38 +1,75 @@
+import React, { Component, useEffect, useState } from "react";
+import Slider from "react-slick";
 import { useNavigate } from "react-router-dom";
-import "../pages/reset.css";
-import "../pages/style.css";
+import "./style.css";
+import "../components/heroside/slick-theme.css";
+import "../components/heroside/slick.css";
+import { useSelector, useDispatch } from "react-redux";
+import { __getMainPost } from "../redux/modules/postSlice";
 
-function Drink() {
+function Drink({rowData}) {
+  const settings = {
+    arrow: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const[mainDrink, setMainDrink] =useState([])
+
+  const mainDrinkList = useSelector((state) => state.details.details.mainPost);
+  console.log("mainDrinkList", mainDrinkList)
+
+  useEffect(() => {
+    dispatch(__getMainPost());
+    console.log("res", mainDrinkList[0])
+  }, [dispatch]);
+
+  const toDrinkList = (post) => {
+    navigate(`/drinkList/drink/detail/${post.id}`)
+  }
+
   return (
-    <>
-      <div className="main_room">
+    <div className="drink_slider">
+      <div className="main_drink">
         <ul className="clearfix">
           <li>
             <h3>
               너도 혼술하니? <br />
               나도 혼술해!
             </h3>
-
-            <h4>자취생의 필수 코스! <br /> 혼술할 때 꿀팁 다 알려줄게 드루와</h4>
-            <button onClick={() => navigate("drinkList/drink")}><h5>글 구경하기</h5> <div className="btn_arrow"></div></button>
-
+            <h4>
+              자취생의 필수 코스! <br /> 혼술할 때 꿀팁 다 알려줄게 드루와
+            </h4>
+            <button onClick={() => navigate("drinkList/drink")}>
+              <h5>글 구경하기</h5> <div className="btn_arrow"></div>
+            </button>
           </li>
-          <li>
-            <div className="room_image"></div>
-            <p>칵테일을 위한 술의 종류 그리고 홈바를 위한 술 추천</p>
-          </li>
-          <li>
-            <div className="room_image"></div>
-            <p>와인이랑 잘 어울리는 음식은 이거지</p>
-          </li>
-          <li>
-            <div className="room_image"></div>
-            <p>연말 파티에 가지 않는 당신에게. 누구보다 맛있게 혼술 즐기는 방법</p>
-          </li>
+          <div className="drink_slide_container">
+            <Slider {...settings}>
+              {mainDrink?.map((rowData, id) => {
+                console.log("row", rowData);
+                <div
+                  className="drink_slide"
+                  key={rowData.id}
+                  onClick={toDrinkList}
+                >
+                  <img src={rowData.imageUrl} alt="image" />
+                  <p>{rowData.title}</p>
+                </div>;
+              })}
+            </Slider>
+          </div>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
+
 export default Drink;
