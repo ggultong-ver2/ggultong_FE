dd553421b797a670a5c59f60c93a01f808985fa2
lidieee <seb384@naver.com>
2023-02-05 22:21:35 +0900

    feat : mainpage categoty slider

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,12 +1,60 @@
+import React, { Component, useEffect, useState } from "react";
+import Slider from "react-slick";
 import { useNavigate } from "react-router-dom";
-import "../pages/reset.css";
-import "../pages/style.css";
+import { useSelector, useDispatch } from "react-redux";
+import { __getMainPost } from "../redux/modules/postSlice";
+import "./style.css";
+import "../components/heroside/slick-theme.css";
+import "../components/heroside/slick.css";
 
 function Recipe() {
+  const settings = {
+    arrow: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+  };
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const postList = useSelector((state) => state.details.details.mainPost);
+  console.log("postList", postList);
+  const [mainList, setMainList] = useState([]);
+
+  useEffect(() => {
+    dispatch(__getMainPost());
+    console.log("res", postList);
+  }, [dispatch]);
+
+  useEffect(() => {
+    const array = postList;
+
+    if (postList) {
+      for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < i * 2; j++) {
+          const mainPageList = {
+            postId: array[1][j].id,
+            title: array[1][j].title,
+            imageUrl: array[1][j].imageUrl,
+          };
+          console.log("arr :", array[1][j]);
+
+          setMainList((old) => [...old, mainPageList]);
+        }
+      }
+    }
+  }, [postList]);
+
+  const topostList = (id) => {
+    navigate(`/mealList/meal/detail/${id}`);
+  };
 
   return (
-    <>
+    <div className="list_slider">
       <div className="main_recipe">
         <ul className="clearfix">
           <li>
@@ -14,26 +62,35 @@ function Recipe() {
               자취하면서 <br />
               만든 음식 모음
             </h3>
-
             <h4>빠르고 간단한 자취 음식 레시피</h4>
-            <button onClick={() => navigate("mealList/meal")}><h5>글 구경하기</h5> <div className="btn_arrow"></div></button>
-
+            <button onClick={() => navigate("mealList/meal")}>
+              <h5>글 구경하기</h5> <div className="btn_arrow"></div>
+            </button>
           </li>
-          <li>
-            <div className="recipe_image"></div>
-            <p>벌레가 나왔을 때 퇴치법 A to Z</p>
-          </li>
-          <li>
-            <div className="recipe_image"></div>
-            <p>벌레가 나왔을 때 퇴치법 A to Z</p>
-          </li>
-          <li>
-            <div className="recipe_image"></div>
-            <p>벌레가 나왔을 때 퇴치법 A to Z</p>
-          </li>
+          <div className="list_slide_container">
+            <div className="slide">
+              <Slider {...settings}>
+                {mainList.map((rowData) => (
+                  <div
+                    className="list_slide"
+                    key={rowData.id}
+                    onClick={() => topostList(rowData.id)}
+                  >
+                    <img
+                      src={rowData.imageUrl}
+                      alt="image"
+                      className="list_slide_image"
+                    />
+                    <p>{rowData.title}</p>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
+
 export default Recipe;
--- src/pages/Recycle.jsx
+++ src/pages/Recycle.jsx
@@ -1,12 +1,60 @@
+import React, { Component, useEffect, useState } from "react";
+import Slider from "react-slick";
 import { useNavigate } from "react-router-dom";
-import "../pages/reset.css";
-import "../pages/style.css";
+import { useSelector, useDispatch } from "react-redux";
+import { __getMainPost } from "../redux/modules/postSlice";
+import "./style.css";
+import "../components/heroside/slick-theme.css";
+import "../components/heroside/slick.css";
 
 function Recycle() {
+  const settings = {
+    arrow: true,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+  };
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const postList = useSelector((state) => state.details.details.mainPost);
+  console.log("postList", postList);
+  const [mainList, setMainList] = useState([]);
+
+  useEffect(() => {
+    dispatch(__getMainPost());
+    console.log("res", postList);
+  }, [dispatch]);
+
+  useEffect(() => {
+    const array = postList;
+
+    if (postList) {
+      for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < i * 2; j++) {
+          const mainPageList = {
+            postId: array[2][j].id,
+            title: array[2][j].title,
+            imageUrl: array[2][j].imageUrl,
+          };
+          console.log("arr :", array[2][j]);
+
+          setMainList((old) => [...old, mainPageList]);
+        }
+      }
+    }
+  }, [postList]);
+
+  const topostList = (id) => {
+    navigate(`/recycleList/recycle/detail/${id}`);
+  };
 
   return (
-    <>
+    <div className="list_slider">
       <div className="main_recycle">
         <ul className="clearfix">
           <li>
@@ -14,30 +62,38 @@ function Recycle() {
               배달음식 남을 때 <br />
               활용 방법
             </h3>
-
             <h4>
-              남은 음식 버리지 말고, 재활용해먹자! <br /> 배달음식보다 맛있는
-              활용음식
+              남은 음식 버리지 말고, 재활용 해먹자! <br /> 배달 음식보다 맛있는
+              활용 음식
             </h4>
             <button onClick={() => navigate("recycleList/recycle")}>
               <h5>글 구경하기</h5> <div className="btn_arrow"></div>
             </button>
           </li>
-          <li>
-            <div className="recycle_image"></div>
-            <p>벌레가 나왔을 때 퇴치법 A to Z</p>
-          </li>
-          <li>
-            <div className="recycle_image"></div>
-            <p>벌레가 나왔을 때 퇴치법 A to Z</p>
-          </li>
-          <li>
-            <div className="recycle_image"></div>
-            <p>벌레가 나왔을 때 퇴치법 A to Z</p>
-          </li>
+          <div className="list_slide_container">
+            <div className="slide">
+              <Slider {...settings}>
+                {mainList.map((rowData) => (
+                  <div
+                    className="list_slide"
+                    key={rowData.id}
+                    onClick={() => topostList(rowData.id)}
+                  >
+                    <img
+                      src={rowData.imageUrl}
+                      alt="image"
+                      className="list_slide_image"
+                    />
+                    <p>{rowData.title}</p>
+                  </div>
+                ))}
+              </Slider>
+            </div>
+          </div>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
+
 export default Recycle;
