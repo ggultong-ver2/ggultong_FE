eeb9b65ac750fbd0f29a86e191534610c977bc14
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2023-02-10 02:20:01 +0900

    fix: 소셜완료

--- src/pages/KakaoLogin.jsx
+++ src/pages/KakaoLogin.jsx
@@ -5,7 +5,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 function KakaoLogin() {
   const location = useLocation();
   const KAKAO_CODE = location.search.split("=")[1];
-  const IP = "sparta-sjl.shop";
+  const IP = "tom-jelly.shop";
   const navigate = useNavigate();
   const getToken = async () => {
     try {
